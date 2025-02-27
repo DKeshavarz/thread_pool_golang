810000b860fe616f459fbb979ebaeee40d160988
Saeed M <mzahry36@gmail.com>
2025-02-28 01:57:06 +0330

    fix : Refactor queue implementation

--- queue/queue.go
+++ queue/queue.go
@@ -7,53 +7,56 @@ import (
 
 type Queue[T any] struct {
 	item []T
-	mutex mutex.Mutex
+	mtx  mutex.Mutex
 }
 
-func New[T any]()(*Queue[T]){
+func New[T any]() *Queue[T] {
 	obj := &Queue[T]{
-		item: make([]T,0),
+		item: make([]T, 0),
 	}
 
-	obj.mutex = mutex.NewAtomicMutex()
-	// obj.mutex = mutex.NewChanMutex()
+	// obj.mtx = mutex.NewAtomicMutex()
+	obj.mtx = mutex.NewChanMutex()
 
 	return obj
 }
 
-func (q *Queue[T])IsEmpty()bool{
-	defer q.mutex.Unlock()
-	q.mutex.Lock()
+func (q *Queue[T]) IsEmpty() bool {
+	q.mtx.Lock()
+	defer q.mtx.Unlock()
 	return len(q.item) <= 0
 }
 
-func (q *Queue[T])Pop()(T,error){
-	ret,err := q.Top()
+func (q *Queue[T]) Pop() (T, error) {
+	var ret T
 
-	defer q.mutex.Unlock()
-	q.mutex.Lock()
+	q.mtx.Lock()
+	defer q.mtx.Unlock()
 
-	if(err != nil){
-		return ret, err
+	if len(q.item) == 0 {
+		return ret, errors.New("empty queue")
 	}
 
+	ret = q.item[0]
 	q.item = q.item[1:]
 	return ret, nil
 }
 
-func (q *Queue[T])Top()(T,error){	
-	if(q.IsEmpty()){
+func (q *Queue[T]) Top() (T, error) {
+	if q.IsEmpty() {
 		var ret T
 		return ret, errors.New("empty queue")
 	}
-	defer q.mutex.Unlock()
-	q.mutex.Lock()
+
+	q.mtx.Lock()
+	defer q.mtx.Unlock()
+
 	return q.item[0], nil
 }
 
-func (q *Queue[T])Push(element T){
-	defer q.mutex.Unlock()
-	q.mutex.Lock()
-	
+func (q *Queue[T]) Push(element T) {
+	q.mtx.Lock()
+	defer q.mtx.Unlock()
+
 	q.item = append(q.item, element)
 }
