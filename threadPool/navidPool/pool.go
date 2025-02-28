e15895cb28ca6038c54a940320e321402776b804
saeedmpro <mzahry36@gmail.com>
2025-03-01 01:48:01 +0330

    feat : enhance task management in RunManger with structured Task type and improved file reading

--- threadPool/navidPool/pool.go
+++ threadPool/navidPool/pool.go
@@ -2,78 +2,121 @@ package navidPool
 
 import (
 	"bufio"
-	"fmt"
 	"log"
 	"os"
+	"sort"
 	"strconv"
+	"strings"
 	"thread_pool/queue"
 	"time"
 )
 
-func RunManger(workerCnt int, fileName string, queueSize int){
-	queue := queue.New[int](queueSize)
+type Task struct {
+	ID          int
+	BurstTime   int
+	ArrivalTime int
+}
+
+func RunManger(workerCnt int, fileName string, queueSize int) {
+	queue := queue.New[Task](queueSize)
 	done := make(chan struct{})
 	finish := make(chan struct{}, workerCnt)
 
 	go fileReader(queue, fileName, done)
-	for i := 1 ; i <= workerCnt ; i++ {
-		go worker(queue, done, i, finish)
+	for i := range workerCnt {
+		go worker(queue, done, finish, i)
 	}
 
-	for i := 0; i < workerCnt; i++ {
+	for range workerCnt {
 		<-finish
 	}
 }
 
-func fileReader(queue *queue.Queue[int], fileName string,done chan struct{}){
-	data,err := os.Open(fileName)
-	defer data.Close()
+func fileReader(q *queue.Queue[Task], fileName string, done chan struct{}) {
+	defer close(done)
 
-	if err != nil{
-		fmt.Println(err)
-		return
+	file, err := os.Open(fileName)
+	if err != nil {
+		log.Fatal("Error opening file:", err)
 	}
+	defer file.Close()
 
-	scanner := bufio.NewScanner(data)
+	var Tasks []Task
+	scanner := bufio.NewScanner(file)
+	idCounter := 1
+
 	for scanner.Scan() {
 		line := scanner.Text()
-		num, err := strconv.Atoi(line)
-
-		if err != nil{
-			fmt.Println(err)
-			return
+		parts := strings.Split(line, " ")
+		if len(parts) != 2 {
+			continue
 		}
-		
-		queue.Push(num)
+
+		arrival, _ := strconv.Atoi(parts[0])
+		burst, _ := strconv.Atoi(parts[1])
+
+		Tasks = append(Tasks, Task{
+			ID:          idCounter,
+			ArrivalTime: arrival,
+			BurstTime:   burst,
+		})
+		idCounter++
 	}
 
-	close(done)
+	sort.Slice(Tasks, func(i, j int) bool {
+		return Tasks[i].ArrivalTime < Tasks[j].ArrivalTime
+	})
+
+	startTime := time.Now()
+	for _, Task := range Tasks {
+		time.Sleep(time.Until(startTime.Add(time.Duration(Task.ArrivalTime) * time.Second)))
+		q.Push(Task)
+	}
+
 }
 
-func heavyCalculation(n int){
-	time.Sleep(time.Second * (time.Duration(n)))
+func processTask(task Task, workerID int) {
+	log.Printf(" Worker %d:\t Started task %d  \t->\t (arrived at %ds)",
+		workerID, task.ID, task.ArrivalTime)
+
+	time.Sleep(time.Duration(task.BurstTime) * time.Second)
+
+	log.Printf(" Worker %d:\t Finished task %d  \t->\t (burst %ds)",
+		workerID, task.ID, task.BurstTime)
 }
 
-func worker(queue *queue.Queue[int], done <-chan struct{}, id int, finish chan <- struct{}){
-	defer func() {
-		finish <- struct{}{}
-	}()
+func worker(q *queue.Queue[Task], done <-chan struct{}, workerDone chan<- struct{}, id int) {
+	defer func() { workerDone <- struct{}{} }()
 
 	for {
-		for !queue.IsEmpty(){
-			val, _ := queue.Pop()
-			log.Printf("  _  ID = %-6d   Start with input -> %-6d \n", id , val)
-			heavyCalculation(val)
-			log.Printf("  _  ID = %-6d   End   with input -> %-6d \n", id , val)
-		}
-
-		select{
+		select {
 		case <-done:
-			return
+			for {
+				task, err := q.Pop()
+				if err != nil {
+					return
+				}
+				processTask(task, id)
+			}
 		default:
-			break;
+			task, err := q.Pop()
+			if err != nil {
+				continue
+			}
+			processTask(task, id)
 		}
 	}
 
-	
+	// for {
+	// 	for !q.IsEmpty() {
+	// 		task, _ := q.Pop()
+	// 		processTask(task, id)
+	// 	}
+	// 	select {
+	// 	case <-done:
+	// 		return
+	// 	default:
+	// 		break
+	// 	}
+	// }
 }
