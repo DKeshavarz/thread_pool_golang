bded32162a0b85b96bd1ef6212a5a0af3bffc2fc
DKeshavarz <dankeshavarz1075@gmail.com>
2025-02-28 06:08:03 +0330

    Fix : fix bug in mutex testing

--- mutex/mutex_test.go
+++ mutex/mutex_test.go
@@ -1,28 +1,28 @@
 package mutex
 
 import (
-	"fmt"
 	"sync"
 	"testing"
 )
 
 func TestMutex(t *testing.T) {
-    mtx := NewChanMutex()
+    mtx := NewAtomicMutex()
 
-	count := 0
+	count, finalCount:= 0, 10000
     var wg sync.WaitGroup
-    for i := 0; i < 10; i++ {
+    for i := 0; i < finalCount; i++ {
 
         wg.Add(1)
         go func(id int) {
 			defer wg.Done()
 			defer mtx.Unlock() 
-            
+
             mtx.Lock() 
             count++
-            fmt.Printf("Goroutine %d after op : count = %d\n", id, count)
         }(i)
     }
-
     wg.Wait()
+    if finalCount != count {
+        t.Fatalf("expected %d, but got %d", finalCount, count)
+    }
 }
