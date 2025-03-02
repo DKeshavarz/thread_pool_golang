9204401a1037294c2f508386ab68567f2696eea0
DKeshavarz <dankeshavarz1075@gmail.com>
2025-03-02 15:36:47 +0330

    Feat : add new test

--- mutex/mutex_test.go
+++ mutex/mutex_test.go
@@ -5,8 +5,30 @@ import (
 	"testing"
 )
 
-func TestMutex(t *testing.T) {
+func TestAtomicMutex(t *testing.T) {
     mtx := NewAtomicMutex()
+
+	count, finalCount:= 0, 10000
+    var wg sync.WaitGroup
+    for i := 0; i < finalCount; i++ {
+
+        wg.Add(1)
+        go func(id int) {
+			defer wg.Done()
+			defer mtx.Unlock() 
+
+            mtx.Lock() 
+            count++
+        }(i)
+    }
+    wg.Wait()
+    if finalCount != count {
+        t.Fatalf("expected %d, but got %d", finalCount, count)
+    }
+}
+
+func TestChanMutex(t *testing.T) {
+    mtx := NewChanMutex()
 
 	count, finalCount:= 0, 10000
     var wg sync.WaitGroup
